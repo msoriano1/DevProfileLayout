b7fe7311de1bcf276926115bc6302d29b7babe75
msoriano1 <matthew.soriano@obf.ateneo.edu>
2019-07-16 16:12:42 +0800

    lesson 04

--- app/src/main/java/com/mattdog/devprofilelayout/MainActivity.kt
+++ app/src/main/java/com/mattdog/devprofilelayout/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.mattdog.devprofilelayout
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        // BitmapFactory.decodeResources: extracts resources to be modified
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        //RoundedBitmapDrawableFactory: converts a bitmap into rounded version
+        rounded.cornerRadius = 15f
+        //round.isCircular = true
+        logo.setImageDrawable(rounded)
+        //converts the drawable image (logo) into the rounded by passing the rounded variable as a parameter
     }
 
     //Blueprint is used for positioning and layout, not colors
@@ -21,5 +34,7 @@ class MainActivity : AppCompatActivity() {
     //Right click set of images then create vertical/horizontal chain to create a consistent contraints
     //Right click set of images then cycle chain to change spacing between images on the chain
     //Right click set of images then Align then Vertical Align to lock in their vertical alignment
+
+
 
 }
